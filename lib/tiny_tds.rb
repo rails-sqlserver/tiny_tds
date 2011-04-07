fc7a0b0d8beba6dacb99c2ad180c77d80dbeaedf
Ken Collins <ken@metaskills.net>
2011-04-06 23:03:58 -0400

    Reorganize the gem file to use git ls-files for test and bin files. Also abstract out version in one place.

--- lib/tiny_tds.rb
+++ lib/tiny_tds.rb
@@ -3,6 +3,7 @@ require 'date'
 require 'bigdecimal'
 require 'rational' unless RUBY_VERSION >= '1.9.2'
 
+require 'tiny_tds/version'
 require 'tiny_tds/error'
 require 'tiny_tds/client'
 require 'tiny_tds/result'
@@ -15,9 +16,4 @@ rescue LoadError
   require 'tiny_tds/tiny_tds'
 end
 
-# = TinyTds
-#
-# Tiny Ruby Wrapper For FreeTDS Using DB-Library
-module TinyTds
-  VERSION = '0.4.3'
-end
+
---
+++ lib/tiny_tds/version.rb
@@ -0,0 +1,3 @@
+module TinyTds
+  VERSION = '0.4.3'
+end

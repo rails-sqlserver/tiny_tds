8f9fda82f4bae32f2c5ab4ea830e95c78433d747
Ken Collins <ken@metaskills.net>
2013-07-10 08:48:58 -0400

    Use both dbsetversion() vs. dbsetlversion. Partially reverts #62.

--- ext/tiny_tds/client.c
+++ ext/tiny_tds/client.c
@@ -270,6 +270,8 @@ static VALUE rb_tinytds_connect(VALUE self, VALUE opts) {
     dbsetlpwd(cwrap->login, StringValuePtr(pass));
   if (!NIL_P(app))
     dbsetlapp(cwrap->login, StringValuePtr(app));
+  if (!NIL_P(version))
+    dbsetlversion(cwrap->login, NUM2INT(version));
   if (!NIL_P(ltimeout))
     dbsetlogintime(NUM2INT(ltimeout));
   if (!NIL_P(timeout))

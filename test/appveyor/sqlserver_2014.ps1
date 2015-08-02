5c2421ac595535a9e3598e09dd5032abfaeb66a8
Ken Collins <ken@metaskills.net>
2015-08-01 22:37:14 -0400

    Execute powershell file.

--- test/appveyor/sqlserver_2014.ps1
+++ test/appveyor/sqlserver_2014.ps1
@@ -4,6 +4,18 @@ $serverName = $env:COMPUTERNAME
 $instanceName = 'SQL2014'
 $smo = 'Microsoft.SqlServer.Management.Smo.'
 $wmi = new-object ($smo + 'Wmi.ManagedComputer')
+
+# For the named instance, on the current computer, for the TCP protocol,
+# loop through all the IPs and configure them to use the standard port
+# of 1433.
+# $uri = "ManagedComputer[@Name='$serverName']/ ServerInstance[@Name='$instanceName']/ServerProtocol[@Name='Tcp']"
+# $Tcp = $wmi.GetSmoObject($uri)
+# foreach ($ipAddress in $Tcp.IPAddresses) {
+#   $ipAddress.IPAddressProperties["TcpDynamicPorts"].Value = ""
+#   $ipAddress.IPAddressProperties["TcpPort"].Value = "1433"
+# }
+# $Tcp.Alter()
+
 # Enable TCP/IP
 $uri = "ManagedComputer[@Name='$serverName']/ServerInstance[@Name='$instanceName']/ServerProtocol[@Name='Tcp']"
 $Tcp = $wmi.GetSmoObject($uri)

3746a78144a22bd720ded5aa902190c5dafe3f1e
Ken Collins <ken@metaskills.net>
2015-11-01 12:45:57 -0500

    Use FreeTDS 0.95.65

--- ext/tiny_tds/extconsts.rb
+++ ext/tiny_tds/extconsts.rb
@@ -5,7 +5,7 @@ ICONV_SOURCE_URI = "http://ftp.gnu.org/pub/gnu/libiconv/libiconv-#{ICONV_VERSION
 OPENSSL_VERSION = ENV['TINYTDS_OPENSSL_VERSION'] || '1.0.2d'
 OPENSSL_SOURCE_URI = "http://www.openssl.org/source/openssl-#{OPENSSL_VERSION}.tar.gz"
 
-FREETDS_VERSION = ENV['TINYTDS_FREETDS_VERSION'] || "0.95.64"
+FREETDS_VERSION = ENV['TINYTDS_FREETDS_VERSION'] || "0.95.65"
 FREETDS_VERSION_INFO = Hash.new { |h,k|
   h[k] = {:files => "ftp://ftp.freetds.org/pub/freetds/stable/freetds-#{k}.tar.bz2"}
 }

eeb340047c57d7023f77843e376cd6ba7afb0771
Ken Collins <ken@metaskills.net>
2015-11-07 16:07:57 -0500

    Allow our `tds_version` to mirror TDSVER env var. Ex '7.3' vs '73'.

--- lib/tiny_tds/client.rb
+++ lib/tiny_tds/client.rb
@@ -1,41 +1,6 @@
 module TinyTds
   class Client
 
-    # From sybdb.h comments:
-    # DBVERSION_xxx are used with dbsetversion()
-    #
-    TDS_VERSIONS_SETTERS = {
-      'unknown' => 0,
-      '46'      => 1,
-      '100'     => 2,
-      '42'      => 3,
-      '70'      => 4,
-      '71'      => 5,
-      '80'      => 5,
-      '72'      => 6,
-      '90'      => 6,
-      '73'      => 7
-    }.freeze
-
-    # From sybdb.h comments:
-    # DBTDS_xxx are returned by DBTDS()
-    # The integer values of the constants are poorly chosen.
-    #
-    TDS_VERSIONS_GETTERS = {
-      0  => {:name => 'DBTDS_UNKNOWN',        :description => 'Unknown'},
-      1  => {:name => 'DBTDS_2_0',            :description => 'Pre 4.0 SQL Server'},
-      2  => {:name => 'DBTDS_3_4',            :description => 'Microsoft SQL Server (3.0)'},
-      3  => {:name => 'DBTDS_4_0',            :description => '4.0 SQL Server'},
-      4  => {:name => 'DBTDS_4_2',            :description => '4.2 SQL Server'},
-      5  => {:name => 'DBTDS_4_6',            :description => '2.0 OpenServer and 4.6 SQL Server.'},
-      6  => {:name => 'DBTDS_4_9_5',          :description => '4.9.5 (NCR) SQL Server'},
-      7  => {:name => 'DBTDS_5_0',            :description => '5.0 SQL Server'},
-      8  => {:name => 'DBTDS_7_0',            :description => 'Microsoft SQL Server 7.0'},
-      9  => {:name => 'DBTDS_7_1/DBTDS_8_0',  :description => 'Microsoft SQL Server 2000'},
-      10 => {:name => 'DBTDS_7_2/DBTDS_9_0',  :description => 'Microsoft SQL Server 2005'},
-      11 => {:name => 'DBTDS_7_3',            :description => 'Microsoft SQL Server 2008'}
-    }.freeze
-
     @@default_query_options = {
       :as => :hash,
       :symbolize_keys => false,
@@ -55,6 +20,7 @@ module TinyTds
       # Most, if not all, iconv encoding names can be found by ruby. Just in case, you can
       # overide this method to return a string name that Encoding.find would work with. Default
       # is to return the passed encoding.
+      #
       def transpose_iconv_encoding(encoding)
         encoding
       end
@@ -68,7 +34,7 @@ module TinyTds
       @query_options = @@default_query_options.dup
       opts[:password] = opts[:password].to_s if opts[:password] && opts[:password].to_s.strip != ''
       opts[:appname] ||= 'TinyTds'
-      opts[:tds_version] = TDS_VERSIONS_SETTERS[opts[:tds_version].to_s] || TDS_VERSIONS_SETTERS['73']
+      opts[:tds_version] = tds_versions_setter(opts)
       opts[:login_timeout] ||= 60
       opts[:timeout] ||= 5
       opts[:encoding] = (opts[:encoding].nil? || opts[:encoding].downcase == 'utf8') ? 'UTF-8' : opts[:encoding].upcase
@@ -99,6 +65,52 @@ module TinyTds
       user, domain = username.split("@")
       "#{user}@#{domain.split('.').first}"
     end
+
+    def tds_versions_setter(opts={})
+      v = opts[:tds_version] || ENV['TDSVER'] || '7.3'
+      TDS_VERSIONS_SETTERS[v.to_s]
+    end
+
+    # From sybdb.h comments:
+    # DBVERSION_xxx are used with dbsetversion()
+    #
+    TDS_VERSIONS_SETTERS = {
+      'unknown' => 0,
+      '46'      => 1,
+      '100'     => 2,
+      '42'      => 3,
+      '70'      => 4,
+      '7.0'     => 4,
+      '71'      => 5,
+      '7.1'     => 5,
+      '80'      => 5,
+      '8.0'     => 5,
+      '72'      => 6,
+      '7.2'     => 6,
+      '90'      => 6,
+      '9.0'     => 6,
+      '73'      => 7,
+      '7.3'     => 7
+    }.freeze
+
+    # From sybdb.h comments:
+    # DBTDS_xxx are returned by DBTDS()
+    # The integer values of the constants are poorly chosen.
+    #
+    TDS_VERSIONS_GETTERS = {
+      0  => {:name => 'DBTDS_UNKNOWN',        :description => 'Unknown'},
+      1  => {:name => 'DBTDS_2_0',            :description => 'Pre 4.0 SQL Server'},
+      2  => {:name => 'DBTDS_3_4',            :description => 'Microsoft SQL Server (3.0)'},
+      3  => {:name => 'DBTDS_4_0',            :description => '4.0 SQL Server'},
+      4  => {:name => 'DBTDS_4_2',            :description => '4.2 SQL Server'},
+      5  => {:name => 'DBTDS_4_6',            :description => '2.0 OpenServer and 4.6 SQL Server.'},
+      6  => {:name => 'DBTDS_4_9_5',          :description => '4.9.5 (NCR) SQL Server'},
+      7  => {:name => 'DBTDS_5_0',            :description => '5.0 SQL Server'},
+      8  => {:name => 'DBTDS_7_0',            :description => 'Microsoft SQL Server 7.0'},
+      9  => {:name => 'DBTDS_7_1/DBTDS_8_0',  :description => 'Microsoft SQL Server 2000'},
+      10 => {:name => 'DBTDS_7_2/DBTDS_9_0',  :description => 'Microsoft SQL Server 2005'},
+      11 => {:name => 'DBTDS_7_3',            :description => 'Microsoft SQL Server 2008'}
+    }.freeze
 
   end
 end

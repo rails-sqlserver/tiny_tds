1cd63d97c8d7d7974a7b118175bbcdacb7df30ba
Ken Collins <ken@metaskills.net>
2016-01-02 12:25:59 -0500

    Debug SYBMSDATETIMEOFFSET

--- ext/tiny_tds/result.c
+++ ext/tiny_tds/result.c
@@ -308,6 +308,7 @@ static VALUE rb_tinytds_result_fetch_row(VALUE self, ID timezone, int symbolize_
                 }
                 case 43: { // SYBMSDATETIMEOFFSET
                   long long numerator = ((long)dr2.second * (long long)1000000000) + (long long)dr2.nanosecond;
+                  rb_warn("SYBMSDATETIMEOFFSET: %d", numerator);
                   VALUE rational_sec = rb_Rational(LONG2NUM(numerator), opt_onebil);
                   val = rb_funcall(rb_cTime, intern_new, 7, INT2NUM(dr2.year), INT2NUM(dr2.month), INT2NUM(dr2.day), INT2NUM(dr2.hour), INT2NUM(dr2.minute), rational_sec, INT2NUM(dr2.tzone*60));
                   break;

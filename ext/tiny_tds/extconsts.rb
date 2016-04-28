7dc3dc78027fd2dc0251ebb42a753f519a76c6c7
Ken Collins <ken@metaskills.net>
2016-04-28 09:45:02 -0400

    Use HTTPS for OpenSSL.

--- ext/tiny_tds/extconsts.rb
+++ ext/tiny_tds/extconsts.rb
@@ -3,7 +3,7 @@ ICONV_VERSION = ENV['TINYTDS_ICONV_VERSION'] || "1.14"
 ICONV_SOURCE_URI = "http://ftp.gnu.org/pub/gnu/libiconv/libiconv-#{ICONV_VERSION}.tar.gz"
 
 OPENSSL_VERSION = ENV['TINYTDS_OPENSSL_VERSION'] || '1.0.2g'
-OPENSSL_SOURCE_URI = "http://www.openssl.org/source/openssl-#{OPENSSL_VERSION}.tar.gz"
+OPENSSL_SOURCE_URI = "https://www.openssl.org/source/openssl-#{OPENSSL_VERSION}.tar.gz"
 
 FREETDS_VERSION = ENV['TINYTDS_FREETDS_VERSION'] || "1.0rc3"
 FREETDS_VERSION_INFO = Hash.new { |h,k|

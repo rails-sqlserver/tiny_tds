3da54f1a075d9700d49e3ef90276889bcf70a586
Ken Collins <ken@metaskills.net>
2016-04-30 20:33:41 -0400

    FreeTDS v1.0rc4

--- ext/tiny_tds/extconsts.rb
+++ ext/tiny_tds/extconsts.rb
@@ -5,11 +5,11 @@ ICONV_SOURCE_URI = "http://ftp.gnu.org/pub/gnu/libiconv/libiconv-#{ICONV_VERSION
 OPENSSL_VERSION = ENV['TINYTDS_OPENSSL_VERSION'] || '1.0.2g'
 OPENSSL_SOURCE_URI = "https://www.openssl.org/source/openssl-#{OPENSSL_VERSION}.tar.gz"
 
-FREETDS_VERSION = ENV['TINYTDS_FREETDS_VERSION'] || "1.0rc3"
+FREETDS_VERSION = ENV['TINYTDS_FREETDS_VERSION'] || "1.0rc4"
 FREETDS_VERSION_INFO = Hash.new { |h,k|
   h[k] = {files: "ftp://ftp.freetds.org/pub/freetds/stable/freetds-#{k}.tar.bz2"}
 }
-FREETDS_VERSION_INFO['1.0rc3'] = {files: 'ftp://ftp.freetds.org/pub/freetds/stable/release_candidates/freetds-1.0rc3.tar.bz2'}
+FREETDS_VERSION_INFO['1.0rc4'] = {files: 'ftp://ftp.freetds.org/pub/freetds/stable/release_candidates/freetds-1.0rc4.tar.bz2'}
 FREETDS_VERSION_INFO['0.99'] = {files: 'ftp://ftp.freetds.org/pub/freetds/current/freetds-dev.0.99.678.tar.gz'}
 FREETDS_VERSION_INFO['0.95'] = {files: 'ftp://ftp.freetds.org/pub/freetds/stable/freetds-0.95.92.tar.gz'}
 FREETDS_SOURCE_URI = FREETDS_VERSION_INFO[FREETDS_VERSION][:files]

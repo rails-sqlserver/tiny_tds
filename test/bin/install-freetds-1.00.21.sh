880cadb558334a8ed4ef456479c10dc8bab07316
Ken Collins <ken@metaskills.net>
2016-11-24 23:25:45 -0500

    [CI] Move over docker freetds setup.

--- test/bin/install-freetds-1.00.21.sh
+++ test/bin/install-freetds-1.00.21.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+
+# docker exec $container locale-gen "en_US.UTF-8"
+# docker exec $container /bin/sh -c "echo 'export LANG=en_US.UTF-8' >> /root/.bashrc"
+# docker exec $container apt-get update
+# docker exec $container apt-get install build-essential -y
+# docker exec $container apt-get install wget -y
+
+# docker cp test/bin/install-freetds-1.00.21.sh $container:~/root
+# docker exec $container /root/install-freetds-1.00.21.sh
+
+
 cd ~
 wget ftp://ftp.freetds.org/pub/freetds/stable/freetds-1.00.21.tar.gz
 tar -xzf freetds-1.00.21.tar.gz

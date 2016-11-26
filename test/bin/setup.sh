a22ae0eff1975fdcf51d560aeccae4ab7ee3c9f0
Ken Collins <ken@metaskills.net>
2016-11-26 15:54:17 -0500

    [CI] Make test/bin/setup.sh more idempotent.

--- test/bin/setup.sh
+++ test/bin/setup.sh
@@ -2,4 +2,14 @@
 set -e
 
 docker pull metaskills/mssql-server-linux-tinytds
-docker run -p 1433:1433 -d metaskills/mssql-server-linux-tinytds && sleep 10
+
+container=$(docker ps -a -q --filter ancestor=metaskills/mssql-server-linux-tinytds)
+if [[ -z $container ]]; then
+  docker run -p 1433:1433 -d metaskills/mssql-server-linux-tinytds && sleep 10
+  exit
+fi
+
+container=$(docker ps -q --filter ancestor=metaskills/mssql-server-linux-tinytds)
+if [[ -z $container ]]; then
+  docker start $container && sleep 10
+fi

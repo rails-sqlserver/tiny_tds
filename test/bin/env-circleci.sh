76b5d797fce92f68c913f67c5432c32b0b53ea22
Ken Collins <ken@metaskills.net>
2016-12-11 15:20:47 -0500

    More dynamic scripting.

---
+++ test/bin/env-circleci.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -x
+set -e
+
+openssl_version=$(ruby -r "./ext/tiny_tds/extconsts.rb" -e "puts OPENSSL_VERSION")
+echo "export OPENSSL_VERSION=$openssl_version" >> ~/.circlerc
+
+freetds_version=$(ruby -r "./ext/tiny_tds/extconsts.rb" -e "puts FREETDS_VERSION")
+echo "export FREETDS_VERSION=$freetds_version" >> ~/.circlerc

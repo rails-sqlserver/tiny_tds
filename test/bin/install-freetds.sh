0e7a4c6bb80961f4445afd201e4d94761590660b
Ken Collins <ken@metaskills.net>
2016-12-11 16:04:31 -0500

    Do not cache any builds.

--- test/bin/install-freetds.sh
+++ test/bin/install-freetds.sh
@@ -7,15 +7,12 @@ if [ -z "$FREETDS_VERSION" ]; then
   FREETDS_VERSION=$(ruby -r "./ext/tiny_tds/extconsts.rb" -e "puts FREETDS_VERSION")
 fi
 
-if [ ! -d build/freetds-$FREETDS_VERSION ]; then
-  cd build
-  wget ftp://ftp.freetds.org/pub/freetds/stable/freetds-$FREETDS_VERSION.tar.gz
-  tar -xzf freetds-$FREETDS_VERSION.tar.gz
-  cd freetds-$FREETDS_VERSION
-  ./configure --prefix=/opt/local \
-              --with-openssl=/opt/local \
-              --with-tdsver=7.3
-  make
-fi
-
+cd build
+wget ftp://ftp.freetds.org/pub/freetds/stable/freetds-$FREETDS_VERSION.tar.gz
+tar -xzf freetds-$FREETDS_VERSION.tar.gz
+cd freetds-$FREETDS_VERSION
+./configure --prefix=/opt/local \
+            --with-openssl=/opt/local \
+            --with-tdsver=7.3
+make
 make install
--- test/bin/install-openssl.sh
+++ test/bin/install-openssl.sh
@@ -7,13 +7,10 @@ if [ -z "$OPENSSL_VERSION" ]; then
   OPENSSL_VERSION=$(ruby -r "./ext/tiny_tds/extconsts.rb" -e "puts OPENSSL_VERSION")
 fi
 
-if [ ! -d build/openssl-$OPENSSL_VERSION ]; then
-  cd build
-  wget https://www.openssl.org/source/openssl-$OPENSSL_VERSION.tar.gz
-  tar -xzf openssl-$OPENSSL_VERSION.tar.gz
-  cd openssl-$OPENSSL_VERSION
-  ./config --prefix=/opt/local
-  make
-fi
-
+cd build
+wget https://www.openssl.org/source/openssl-$OPENSSL_VERSION.tar.gz
+tar -xzf openssl-$OPENSSL_VERSION.tar.gz
+cd openssl-$OPENSSL_VERSION
+./config --prefix=/opt/local
+make
 make install

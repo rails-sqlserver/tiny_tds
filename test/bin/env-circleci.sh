184408b949f889db07e7eecfde73f42bdf5b5c15
Ken Collins <ken@metaskills.net>
2016-12-11 15:39:12 -0500

    Foo.

--- test/bin/env-circleci.sh
+++ test/bin/env-circleci.sh
@@ -3,8 +3,8 @@
 set -x
 set -e
 
-openssl_version=$(ruby -r "./ext/tiny_tds/extconsts.rb" -e "puts OPENSSL_VERSION")
+openssl_version=$(ruby -r "$HOME/ext/tiny_tds/extconsts.rb" -e "puts OPENSSL_VERSION")
 echo "export OPENSSL_VERSION=$openssl_version" >> ~/.circlerc
 
-freetds_version=$(ruby -r "./ext/tiny_tds/extconsts.rb" -e "puts FREETDS_VERSION")
+freetds_version=$(ruby -r "$HOME/ext/tiny_tds/extconsts.rb" -e "puts FREETDS_VERSION")
 echo "export FREETDS_VERSION=$freetds_version" >> ~/.circlerc

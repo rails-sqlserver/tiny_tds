d89566107ed954ec7c47952de7ac764926f51263
Andrew Vit <andrew@avit.ca>
2017-02-16 07:36:52 -0800

    Use alternate source for current freetds version
    ftp://ftp.freetds.org is down. Bumping the library version to 1.00.26 in order
    to use an alternate source available from fossies.org (this source is used by
    homebrew-core).

--- ext/tiny_tds/extconsts.rb
+++ ext/tiny_tds/extconsts.rb
@@ -5,10 +5,11 @@ ICONV_SOURCE_URI = "http://ftp.gnu.org/pub/gnu/libiconv/libiconv-#{ICONV_VERSION
 OPENSSL_VERSION = ENV['TINYTDS_OPENSSL_VERSION'] || '1.0.2j'
 OPENSSL_SOURCE_URI = "https://www.openssl.org/source/openssl-#{OPENSSL_VERSION}.tar.gz"
 
-FREETDS_VERSION = ENV['TINYTDS_FREETDS_VERSION'] || "1.00.21"
+FREETDS_VERSION = ENV['TINYTDS_FREETDS_VERSION'] || "1.00.26"
 FREETDS_VERSION_INFO = Hash.new { |h,k|
   h[k] = {files: "ftp://ftp.freetds.org/pub/freetds/stable/freetds-#{k}.tar.bz2"}
 }
+FREETDS_VERSION_INFO['1.00.26'] = {files: 'https://fossies.org/linux/privat/freetds-1.00.26.tar.gz'}
 FREETDS_VERSION_INFO['1.00'] = {files: 'ftp://ftp.freetds.org/pub/freetds/stable/freetds-1.00.tar.bz2'}
 FREETDS_VERSION_INFO['0.99'] = {files: 'ftp://ftp.freetds.org/pub/freetds/current/freetds-dev.0.99.678.tar.gz'}
 FREETDS_VERSION_INFO['0.95'] = {files: 'ftp://ftp.freetds.org/pub/freetds/stable/freetds-0.95.92.tar.gz'}

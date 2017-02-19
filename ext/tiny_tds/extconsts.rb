268a0f51fff3b5305dbe55cfea1ecf9ed610e202
Ken Collins <ken@metaskills.net>
2017-02-18 22:01:17 -0500

    Use OpenSSL v1.1.0e & FreeTDS v1.00.27
    Fixes issue #310

--- ext/tiny_tds/extconsts.rb
+++ ext/tiny_tds/extconsts.rb
@@ -2,10 +2,10 @@
 ICONV_VERSION = ENV['TINYTDS_ICONV_VERSION'] || "1.14"
 ICONV_SOURCE_URI = "http://ftp.gnu.org/pub/gnu/libiconv/libiconv-#{ICONV_VERSION}.tar.gz"
 
-OPENSSL_VERSION = ENV['TINYTDS_OPENSSL_VERSION'] || '1.0.2j'
+OPENSSL_VERSION = ENV['TINYTDS_OPENSSL_VERSION'] || '1.1.0e'
 OPENSSL_SOURCE_URI = "https://www.openssl.org/source/openssl-#{OPENSSL_VERSION}.tar.gz"
 
-FREETDS_VERSION = ENV['TINYTDS_FREETDS_VERSION'] || "1.00.21"
+FREETDS_VERSION = ENV['TINYTDS_FREETDS_VERSION'] || "1.00.27"
 FREETDS_VERSION_INFO = Hash.new { |h,k|
   h[k] = {files: "ftp://ftp.freetds.org/pub/freetds/stable/freetds-#{k}.tar.bz2"}
 }

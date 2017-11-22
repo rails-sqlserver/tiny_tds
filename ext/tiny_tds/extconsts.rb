ee930e360a50d48c25168943e0f38c7b5fa3bcc5
Ken Collins <ken@metaskills.net>
2017-11-22 10:38:04 -0500

    Revert "Test FreeTDS to 1.00.77"
    This reverts commit 2cc3e75edd028b528582eb44648efdfb750fde7e.

--- ext/tiny_tds/extconsts.rb
+++ ext/tiny_tds/extconsts.rb
@@ -5,7 +5,7 @@ ICONV_SOURCE_URI = "http://ftp.gnu.org/pub/gnu/libiconv/libiconv-#{ICONV_VERSION
 OPENSSL_VERSION = ENV['TINYTDS_OPENSSL_VERSION'] || '1.1.0e'
 OPENSSL_SOURCE_URI = "https://www.openssl.org/source/openssl-#{OPENSSL_VERSION}.tar.gz"
 
-FREETDS_VERSION = ENV['TINYTDS_FREETDS_VERSION'] || "1.00.77"
+FREETDS_VERSION = ENV['TINYTDS_FREETDS_VERSION'] || "1.00.27"
 FREETDS_VERSION_INFO = Hash.new { |h,k|
   h[k] = {files: "http://www.freetds.org/files/stable/freetds-#{k}.tar.bz2"}
 }

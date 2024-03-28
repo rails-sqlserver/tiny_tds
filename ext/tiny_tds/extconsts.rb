1ebeae6bb400f1d65310cf85744cb373f1c62d02
Andy Pfister <andy.pfister@hey.com>
2024-03-28 21:02:19 +0100

    Actually drop support for FreeTDS < 1.0
    `DBSETUTF16` has only been introduced with v1.0.

--- ext/tiny_tds/extconsts.rb
+++ ext/tiny_tds/extconsts.rb
@@ -5,11 +5,5 @@ ICONV_SOURCE_URI = "http://ftp.gnu.org/pub/gnu/libiconv/libiconv-#{ICONV_VERSION
 OPENSSL_VERSION = ENV['TINYTDS_OPENSSL_VERSION'] || '1.1.1s'
 OPENSSL_SOURCE_URI = "https://www.openssl.org/source/openssl-#{OPENSSL_VERSION}.tar.gz"
 
-FREETDS_VERSION = ENV['TINYTDS_FREETDS_VERSION'] || "1.1.24"
-FREETDS_VERSION_INFO = Hash.new { |h,k|
-  h[k] = {files: "http://www.freetds.org/files/stable/freetds-#{k}.tar.bz2"}
-}
-FREETDS_VERSION_INFO['1.00'] = {files: 'http://www.freetds.org/files/stable/freetds-1.00.tar.bz2'}
-FREETDS_VERSION_INFO['0.99'] = {files: 'http://www.freetds.org/files/current/freetds-dev.0.99.678.tar.gz'}
-FREETDS_VERSION_INFO['0.95'] = {files: 'http://www.freetds.org/files/stable/freetds-0.95.92.tar.gz'}
-FREETDS_SOURCE_URI = FREETDS_VERSION_INFO[FREETDS_VERSION][:files]
+FREETDS_VERSION = ENV['TINYTDS_FREETDS_VERSION'] || '1.1.24'
+FREETDS_SOURCE_URI = "http://www.freetds.org/files/stable/freetds-#{FREETDS_VERSION}.tar.bz2"

3dff397386a847cd7f4d254779b67c987a2097ea
Andy Pfister <andy.pfister@hey.com>
2025-01-09 13:54:34 +0100

    Provide `sysconfdir` to FreeTDS
    You can use this parameter to specify a `freetds.conf` there. per default, it will use the build path, which will be some obscure thing on GitHub Actions. Although most people will customize this location at runtime using environment variables, it still makes sense to provide sensible defaults.

--- ext/tiny_tds/extconf.rb
+++ ext/tiny_tds/extconf.rb
@@ -115,6 +115,7 @@ if gem_platform=with_config("cross-build")
 
     recipe.configure_options << "--with-openssl=#{openssl_recipe.path}"
     recipe.configure_options << "--with-libiconv-prefix=#{libiconv_recipe.path}"
+    recipe.configure_options << "--sysconfdir=#{MiniPortile.windows? ? "C:/Sites" : "/usr/local/etc"}"
 
     recipe.gem_platform = gem_platform
     recipe.cook_and_activate

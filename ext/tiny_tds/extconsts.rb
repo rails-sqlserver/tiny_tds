03e49ce09305f9fc5de8ddbbd6013069040cd1b0
Andy Pfister <andy.pfister@hey.com>
2025-08-18 22:07:50 +0200

    Update FreeTDS to v1.5.4

--- ext/tiny_tds/extconsts.rb
+++ ext/tiny_tds/extconsts.rb
@@ -4,5 +4,5 @@ ICONV_SOURCE_URI = "http://ftp.gnu.org/pub/gnu/libiconv/libiconv-#{ICONV_VERSION
 OPENSSL_VERSION = ENV["TINYTDS_OPENSSL_VERSION"] || "3.5.0"
 OPENSSL_SOURCE_URI = "https://www.openssl.org/source/openssl-#{OPENSSL_VERSION}.tar.gz"
 
-FREETDS_VERSION = ENV["TINYTDS_FREETDS_VERSION"] || "1.5.1"
+FREETDS_VERSION = ENV["TINYTDS_FREETDS_VERSION"] || "1.5.4"
 FREETDS_SOURCE_URI = "http://www.freetds.org/files/stable/freetds-#{FREETDS_VERSION}.tar.bz2"
